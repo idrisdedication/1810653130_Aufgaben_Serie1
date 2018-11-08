ee987e2a36d66f8701f394c3205c120f8d9eff47
unknown <you@example.com>
2018-11-08 15:16:23 +0100

    Kommentare hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main
 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Abdullatif Atalay ");
+    public static void main(String[] args) {    // public weil von außerhalb der Klasse auf sie zugegriffen wird
+                                                // static weil sie aufgerufen wird ohne dass vorher ein Objekt einer Klasse gebildet wurde
+                                                // void weil sie keinen Rückgabewert besitzt
+                                                // der Parameter String[] args, der auch String args[] geschrieben werden darf, ist ein Array, das die vom Aufruf entgegen genommenen Kommandozeilenparameter enthält
+        System.out.println("Hello World"); // bei System.out.println wird nach der Ausgabe ein Absatz eingefügt
+                                                             // Hello World wird auf der Konsole ausgegeben
+        System.out.println("Abdullatif Atalay "); // Abdullatif Atalay wird auf auf der Konsole ausgegeben
     }
 }
--- src/Package2/NeueKlasse.java
+++ src/Package2/NeueKlasse.java
@@ -2,7 +2,7 @@ package Package2;
 
 public class NeueKlasse
 {
-    public static void main(String[] args) {
-        System.out.println("FH Kufstein Tirol");
+    public static void main(String[] args) { // Kommentare->*siehe Main.java
+        System.out.println("FH Kufstein Tirol"); // FH Kufstein wird auf der Konsole ausgegeben
     }
 }
